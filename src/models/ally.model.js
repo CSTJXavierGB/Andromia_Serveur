680c2ac9da297c9b2b788322a0618d39c853e829
CSTJXavier <6226374@cstj.qc.ca>
2025-10-27 16:14:40 -0400

    Création du model d'un 'ally'

---
+++ src/models/ally.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import { strict } from 'assert';
+import { timeStamp } from 'console';
+
+const allySchema = mongoose.Schema(
+    {
+        uuid: { type: String, required: true, unique: true, default: () => crypto.randomUUID() },
+        name: { type: String },
+        affinity: { type: String },
+        kernel : [ { type: String } ],
+        life: { type: Number},
+        speed: {
+            low: { type: Number },
+            high: { type: Number },
+        },
+        power: {
+            low: { type: Number },
+            high: { type: Number },
+        },
+        shield: {
+            low: { type: Number },
+            high: { type: Number },
+        }
+    },
+    {
+        collection: 'allies',
+        strict: 'throw',
+        timeStamp: true
+    }
+);
+
+const Ally = mongoose.model('Ally', allySchema);
+
+export { Ally };
