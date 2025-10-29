d2e8e926b7d0d6325eb685c997b21ce7f3470df0
William <2246747@cstj.qc.ca>
2025-10-29 14:02:00 -0400

    Basic login without JWT implementation

--- src/repositories/explorer.repository.js
+++ src/repositories/explorer.repository.js
@@ -1,9 +1,40 @@
 import { Explorer } from '../models/explorer.model.js';
+import argon from 'argon2';
+import HttpErrors from 'http-errors';
 
 class ExplorerRepository {
-    create(explorer) {
-        return Explorer.create(explorer);
+    async create(explorer) {
+
+        try {
+            explorer.password = await argon.hash(explorer.password);
+            return Explorer.create(explorer);
+        } catch (err) {
+            throw new Error('Error hashing password');
+        }
+
     }
+
+    async validatePassword(password, explorer) {
+        return await argon.verify(explorer.password, password)
+    }
+
+    async login(username, password) {
+        const explorer = await this.retrieveByCredentials(username);
+        if (!explorer){
+
+            throw HttpErrors.Unauthorized();
+
+        } 
+
+        if(!await this.validatePassword(password, explorer)){
+            throw HttpErrors.Unauthorized();
+        }
+
+        return explorer;
+
+
+    }
+
 
     retrieveAll() {
         return Explorer.find().populate('allies');
@@ -13,9 +44,19 @@ class ExplorerRepository {
         return Explorer.findOne({ uuid }).populate('allies');
     }
 
+    retrieveByCredentials(username) {
+        return Explorer.findOne({ username });
+    }
+
     transform(explorer) {
         explorer.href = `${process.env.BASE_URL}/explorers/${explorer.uuid}`;        
 
+        delete explorer.password;
+        if (explorer.vault && explorer.vault.elements) {
+            explorer.vault.elements.forEach(element => {
+                delete element._id;
+            });
+        }
         delete explorer.uuid;
         delete explorer._id;
         delete explorer.__v;
---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import HttpErrors from 'http-errors';
+
+import explorerRepository from '../repositories/explorer.repository.js';
+
+// import TokenController from '../controllers/token.controller.js';
+// const tokenController = new TokenController()
+
+// import { guardAuthorizationJWT, revokeAuthorization } from '../middlewares/authorization.jwt.js';
+
+const router = express.Router();
+
+router.post('/', login);
+// router.delete('/', logout);
+
+async function login(req, res, next) {
+    try {
+
+        const {username, password} = req.body;
+
+        let explorer = await explorerRepository.login(username,password)
+
+        // // TODO : JWT Tokens
+        // const tokens = explorerRepository.generateJWT(explorer.uuid);
+
+        explorer = explorer.toObject({getters:false, virtuals: false});
+        explorer = explorerRepository.transform(explorer)
+
+        // TODO : add tokens in response
+        res.status(201).json({explorer});
+
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+// async function logout(req, res, next) {
+//     try {
+//        await tokenController.invalidate(req.body.refreshToken)
+//        res.status(204).end();
+//     } catch(err) {
+//         return next(err);
+//     } 
+// }
+
+export default router;
