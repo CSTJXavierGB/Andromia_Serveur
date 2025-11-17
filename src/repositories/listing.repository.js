5bc3839edcb8d997dc5dafc8fb0f185186700c51
William <2246747@cstj.qc.ca>
2025-11-17 16:52:10 -0500

    Basic implementation of listings post
    TODO : validate ally belong to explorer

--- src/repositories/listing.repository.js
+++ src/repositories/listing.repository.js
@@ -1,8 +1,49 @@
 import Listing from '../models/listing.model.js';
+import alliesRepository from './ally.repository.js';
 import explorerRepository from './explorer.repository.js';
 
 
 class ListingRepository {
+
+    async create(allyUUID, explorerUUID, inox) {
+        try {
+
+
+            let ally = await alliesRepository.retrieveByUUID(allyUUID);
+            let explorer = await explorerRepository.retrieveOne(explorerUUID);
+
+            const listing = await Listing.create({
+                seller: explorer._id,
+                ally: ally._id,
+                inox: inox
+            });
+
+            await this.#handlePopulateOption(listing);
+            return listing;
+        } catch (err) {
+            throw new Error('Error creating listing: ' + err.message);
+        }
+    }
+
+    transform(listing, options = {}) {
+        listing.href = `${process.env.BASE_URL}/listings/${listing.uuid}`;
+        delete listing._id;
+        delete listing.__v;
+        delete listing.seller._id;
+        delete listing.ally._id;
+        delete listing.uuid;
+        delete listing.updatedAt;
+        return listing;
+    }
+
+    //Fonction privé pour géré les populates
+    async #handlePopulateOption(query, options = {}) {
+
+            await query.populate('seller', 'uuid username');
+            await query.populate('ally', 'uuid name');
+
+        return query;
+    }
 
 }
 
---
+++ src/routes/listings.routes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import HttpErrors from 'http-errors';
+
+
+import { guardAuthorizationJWT } from '../middlewares/authorization.jwt.js';
+import listingRepository from '../repositories/listing.repository.js';
+
+const router = express.Router();
+
+router.post('/', guardAuthorizationJWT, post);
+
+async function post(req, res, next) {
+    try {
+
+        const allyUUID = req.body.allyHref.split('/').pop();
+        const explorerUuid = req.auth.uuid;
+        const inox = req.body.inox;
+        
+
+        let listing = await listingRepository.create(allyUUID,explorerUuid, inox);
+
+        listing = listing.toObject({ getters: false, virtuals: false });
+
+        
+         listing = listingRepository.transform(listing);
+
+
+         if (req.query._body && req.query._body === 'false') {
+            res.status(204).end();
+        } else {
+            res.status(201).json({ listing });
+        }
+
+    } catch (err) {
+        return next(err);
+    }
+}
+
+
+
+export default router;
